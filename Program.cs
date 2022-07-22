a2eb91b59ae8a29db63ada50f9c2800c5040c4d5
Jeremaine Osia <jeremaine@primalux.com.sg>
2022-07-22 23:43:37 +0800

    Service Response witth Generics

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+global using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +10,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<ICharacterService, CharacterService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+  public class CharacterService : ICharacterService
+  {
+    private static List<Character> characters = new List<Character>{
+      new Character(),
+      new Character { Id = 1, Name = "Champoy "},
+    };
+    public async Task<ServiceResponse<List<Character>>> AddCharacter(Character character)
+    {
+      var serviceResponse = new ServiceResponse<List<Character>>();
+      characters.Add(character);
+      serviceResponse.Data = characters;
+      return serviceResponse;
+    }
+
+    public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+    {
+      return new ServiceResponse<List<Character>> { Data = characters };
+    }
+
+    public async Task<ServiceResponse<Character>> GetCharacterById(int id)
+    {
+      var serviceResponse = new ServiceResponse<Character>();
+      var character = characters.FirstOrDefault(c => c.Id == id);
+      serviceResponse.Data = character;
+      return serviceResponse;
+    }
+  }
+}
